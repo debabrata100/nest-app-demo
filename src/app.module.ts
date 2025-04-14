96e1d07fec362d1a2d5f3d3858ee8bef29812e81
Debabrata <m.debabrata100@gmail.com>
2025-04-14 12:04:46 +0530

    database config now read from env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -12,15 +12,19 @@ import { LoggerMiddleware } from './logger/logger.middleware';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'nestuser',
-      password: 'nestpass',
-      database: 'nestdb',
-      autoLoadEntities: true,
-      synchronize: true, // NOTE: disable in production
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // disable in production
+      }),
     }),
     UserModule,
     AuthModule,
